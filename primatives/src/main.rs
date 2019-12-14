67143e646ca9083d951fc721377370844e728611
Taylor Spencer <tayspen72@gmail.com>
2019-12-13 21:41:14 -0700

    Primatives: Tuples

--- primatives/src/main.rs
+++ primatives/src/main.rs
@@ -1,77 +1,129 @@
-fn main() {
-    /*
-      Scalar Types
-        signed integers: i8, i16, i32, i64, i128 and isize (pointer size)
-        unsigned integers: u8, u16, u32, u64, u128 and usize (pointer size)
-        floating point: f32, f64
-        char Unicode scalar values like 'a', 'α' and '∞' (4 bytes each)
-        bool either true or false
-        and the unit type (), whose only possible value is an empty tuple: ()
+// Tuples can be used as function arguments and as return values
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+  // `let` can be used to bind the members of a tuple to variables
+  let (integer, boolean) = pair;
 
-      Compound Types
-        arrays like [1, 2, 3]
-        tuples like (1, true)
+  (boolean, integer)
+}
+
+// The following struct is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+  /*
+    Scalar Types
+      signed integers: i8, i16, i32, i64, i128 and isize (pointer size)
+      unsigned integers: u8, u16, u32, u64, u128 and usize (pointer size)
+      floating point: f32, f64
+      char Unicode scalar values like 'a', 'α' and '∞' (4 bytes each)
+      bool either true or false
+      and the unit type (), whose only possible value is an empty tuple: ()
+
+    Compound Types
+      arrays like [1, 2, 3]
+      tuples like (1, true)
+  */
+
+  // Variables can be type annotated.
+  let logical: bool = true;
+  println!("logical: {}", logical);
+
+  let a_float: f64 = 1.0;  // Regular annotation
+  let an_integer   = 5i32; // Suffix annotation
+  println!("a_float: {}", a_float);
+  println!("an_integer: {}", an_integer);
+
+  // Or a default will be used.
+  let default_float   = 3.0; // `f64`
+  let default_integer = 7;   // `i32`
+  println!("default_float: {}", default_float);
+  println!("default_integer: {}", default_integer);
+
+  // A type can also be inferred from context 
+  let mut inferred_type = 12; // Type i64 is inferred from another line
+  println!("inferred_type: {}", inferred_type);
+  inferred_type = 4294967296i64;
+  println!("inferred_type: {}", inferred_type);
+
+  // A mutable variable's value can be changed.
+  let mut mutable = 12; // Mutable `i32`
+  println!("mutable: {}", mutable);
+  mutable = 21;
+  println!("mutable: {}", mutable);
+
+  // Error! The type of a variable can't be changed.
+  // mutable = true;
+
+  // Variables can be overwritten with shadowing.
+  let mutable = true;
+  println!("mutable: {}", mutable);
+
+  /*
+    Integers can, alternatively, be expressed using hexadecimal, octal or 
+    binary notation using these prefixes respectively: 0x, 0o or 0b.
     */
 
-    // Variables can be type annotated.
-    let logical: bool = true;
-    println!("logical: {}", logical);
+  // Integer addition
+  println!("1 + 2 = {}", 1u32 + 2);
 
-    let a_float: f64 = 1.0;  // Regular annotation
-    let an_integer   = 5i32; // Suffix annotation
-    println!("a_float: {}", a_float);
-    println!("an_integer: {}", an_integer);
+  // Integer subtraction
+  println!("1 - 2 = {}", 1i32 - 2);
+  // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
 
-    // Or a default will be used.
-    let default_float   = 3.0; // `f64`
-    let default_integer = 7;   // `i32`
-    println!("default_float: {}", default_float);
-    println!("default_integer: {}", default_integer);
+  // Short-circuiting boolean logic
+  println!("true AND false is {}", true && false);
+  println!("true OR false is {}", true || false);
+  println!("NOT true is {}", !true);
 
-    // A type can also be inferred from context 
-    let mut inferred_type = 12; // Type i64 is inferred from another line
-    println!("inferred_type: {}", inferred_type);
-    inferred_type = 4294967296i64;
-    println!("inferred_type: {}", inferred_type);
+  // Bitwise operations
+  println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+  println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+  println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+  println!("1 << 5 is {}", 1u32 << 5);
+  println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 
-    // A mutable variable's value can be changed.
-    let mut mutable = 12; // Mutable `i32`
-    println!("mutable: {}", mutable);
-    mutable = 21;
-    println!("mutable: {}", mutable);
+  // Use underscores to improve readability!
+  println!("One million is written as {}", 1_000_000u32);
     
-    // Error! The type of a variable can't be changed.
-    // mutable = true;
-    
-    // Variables can be overwritten with shadowing.
-    let mutable = true;
-    println!("mutable: {}", mutable);
+  // A tuple with a bunch of different types
+  let long_tuple = (1u8, 2u16, 3u32, 4u64,
+    -1i8, -2i16, -3i32, -4i64,
+    0.1f32, 0.2f64,
+    'a', true);
 
-    /*
-      Integers can, alternatively, be expressed using hexadecimal, octal or 
-      binary notation using these prefixes respectively: 0x, 0o or 0b.
-     */
+  // Values can be extracted from the tuple using tuple indexing
+  println!("long tuple first value: {}", long_tuple.0);
+  println!("long tuple second value: {}", long_tuple.1);
 
-    // Integer addition
-    println!("1 + 2 = {}", 1u32 + 2);
+  // Tuples can be tuple members
+  let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-    // Integer subtraction
-    println!("1 - 2 = {}", 1i32 - 2);
-    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+  // Tuples are printable
+  println!("tuple of tuples: {:?}", tuple_of_tuples);
 
-    // Short-circuiting boolean logic
-    println!("true AND false is {}", true && false);
-    println!("true OR false is {}", true || false);
-    println!("NOT true is {}", !true);
+  // But long Tuples cannot be printed
+  // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+  // println!("too long tuple: {:?}", too_long_tuple);
+  // TODO ^ Uncomment the above 2 lines to see the compiler error
 
-    // Bitwise operations
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+  let pair = (1, true);
+  println!("pair is {:?}", pair);
 
-    // Use underscores to improve readability!
-    println!("One million is written as {}", 1_000_000u32);
-    
+  println!("the reversed pair is {:?}", reverse(pair));
+
+  // To create one element tuples, the comma is required to tell them apart
+  // from a literal surrounded by parentheses
+  println!("one element tuple: {:?}", (5u32,));
+  println!("just an integer: {:?}", (5u32));
+
+  //tuples can be destructured to create bindings
+  let tuple = (1, "hello", 4.5, true);
+
+  let (a, b, c, d) = tuple;
+  println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+  let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+  println!("{:?}", matrix);
+
 }
