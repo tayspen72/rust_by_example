d0009c5596dd1fe77ef2a670565f0fb6761079e4
Taylor Spencer <tayspen72@gmail.com>
2019-12-19 22:33:40 -0700

    Chapter 5: Types complete

--- 05_types/src/main.rs
+++ 05_types/src/main.rs
@@ -47,9 +47,79 @@ fn casting(){
     println!(" 232 as a i8 is : {}", 232 as i8);
 }
 
+fn literals(){
+    // Suffixed literals, their types are known at initialization
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
+
+    // Unsuffixed literal, their types depend on how they are used
+    let i = 1;
+    let f = 1.0;
+
+    // `size_of_val` returns the size of a variable in bytes
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+
+    /*
+    fun(&foo) is used to pass an argument to a function by reference,
+    rather than by value (fun(foo)). For more details see borrowing.
+    
+    std::mem::size_of_val is a function, but called with its full path. 
+    Code can be split in logical units called modules. In this case, the 
+    size_of_val function is defined in the mem module, and the mem module 
+    is defined in the std crate. For more details, see modules and crates.
+    */
+}
+
+fn interference(){
+    // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // At this point the compiler doesn't know the exact type of `vec`, it
+    // just knows that it's a vector of something (`Vec<_>`).
+
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
+    // TODO ^ Try commenting out the `vec.push(elem)` line
+
+    println!("{:?}", vec);
+}
+
+// `NanoSecond` is a new name for `u64`.
+type NanoSecond = u64;
+type Inch = u64;
+
+// Use an attribute to silence warning.
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+// TODO ^ Try removing the attribute
+
+fn aliasing(){
+    // `NanoSecond` = `Inch` = `u64_t` = `u64`.
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches);
+}
 
 fn main() {
     casting();
 
+    literals();
 
+    interference();
+
+    aliasing();
 }
