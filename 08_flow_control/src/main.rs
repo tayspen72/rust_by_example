93c8ec4a0bda01a24a838700fd1f5e5d17c9e5d6
Taylor Spencer <tspencer@prismview.com>
2019-12-20 14:46:39 -0700

    08 Flow Control: setion 4 complete

--- 08_flow_control/src/main.rs
+++ 08_flow_control/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unreachable_code)]
+
 fn the_if_else(){
     //unlike other languages, the boolean condition does not need parenthesis
     let n = 5;
@@ -28,11 +30,150 @@ fn the_if_else(){
     println!("{} -> {}", n, big_n);
 }
 
-// fn the_loop();
+fn the_loop(){
+    let mut count = 0u32;
 
-// fn the_while();
+    println!("Let's count until infinity!");
 
-// fn the_for_and_range();
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of this iteration of the loop
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // Exit this loop
+            break;
+        }
+    }
+
+    //the building blocks for a goto....
+    //but also has potential to make intention very explicit
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+
+    //remember scope with loops
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
+
+fn the_while(){
+    // A counter variable
+    let mut n = 1;
+
+    // Loop while `n` is less than 101
+    while n < 16 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        // Increment counter
+        n += 1;
+    }
+}
+
+fn the_for_and_range(){
+    // `n` will take the values: 1, 2, ..., 14 in each iteration
+    for n in 1..15 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // `n` will take the values: 1, 2, ..., 15 in each iteration
+    // IE: endpoint inclusive
+    for n in 1..=15 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    //for loop using iterators
+
+    //default iterator, non destructive and does not modify the data
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    println!("names: {:?}", names);
+
+    //destructive iterator, will contain exact data but destroy collection
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    //cannot print because 'names' have been borrowed into name iterator
+    // println!("names: {:?}", names);
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    //borrows item in collection - allows for modification
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);
+
+}
 
 // fn the_match();
 
@@ -43,11 +184,11 @@ fn the_if_else(){
 fn main() {
     the_if_else();
 
-    // the_loop();
+    the_loop();
 
-    // the_while();
+    the_while();
 
-    // the_for_and_range();
+    the_for_and_range();
 
     // the_match();
 
