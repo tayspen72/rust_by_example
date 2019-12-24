60c4fd6c284a47b6f5bc2cec04fcdc6794ffbcb9
Taylor Spencer <tayspen72@gmail.com>
2019-12-23 23:32:59 -0700

    08 Flow Control complete

--- 08_flow_control/src/main.rs
+++ 08_flow_control/src/main.rs
@@ -175,11 +175,261 @@ fn the_for_and_range(){
 
 }
 
-// fn the_match();
+fn match_tuples(){
+    let pair = (0, -2);
+    // TODO ^ Try different values for `pair`
 
-// fn the_if_loop();
+    print!("Tell me about {:?}: ", pair);
+    // Match can be used to destructure a tuple
+    match pair {
+        // Destructure the second
+        (0, y) => println!("First is `0` and `y` is `{:?}`", y),
+        (x, 0) => println!("`x` is `{:?}` and last is `0`", x),
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+}
 
-// fn the_while_let();
+// `allow` required to silence warnings because only
+// one variant is used.
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn match_enums(){
+    // let color = Color::RGB(122, 17, 40);
+    let color = Color::Blue;
+    // let color = Color::CMYK(1, 2, 3, 4);
+
+    print!("What color is it?: ");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+        // Don't need another arm because all variants have been examined
+    }
+}
+
+fn match_pointers_and_references(){
+    // Assign a reference of type `i32`. The `&` signifies there
+    // is a reference being assigned.
+    let reference = &4;
+
+    match reference {
+        // If `reference` is pattern matched against `&val`, it results
+        // in a comparison like:
+        // `&i32`
+        // `&val`
+        // ^ We see that if the matching `&`s are dropped, then the `i32`
+        // should be assigned to `val`.
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // To avoid the `&`, you dereference before matching.
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+    // What if you don't start with a reference? `reference` was a `&`
+    // because the right side was already a reference. This is not
+    // a reference because the right side is not one.
+    let _not_a_reference = 3;
+
+    // Rust provides `ref` for exactly this purpose. It modifies the
+    // assignment so that a reference is created for the element; this
+    // reference is assigned.
+    let ref _is_a_reference = 3;
+
+    // Accordingly, by defining 2 values without references, references
+    // can be retrieved via `ref` and `ref mut`.
+    let value = 5;
+    let mut mut_value = 6;
+
+    // Use `ref` keyword to create a reference.
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    // Use `ref mut` similarly.
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can
+            // add anything to it.
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        },
+    }
+}
+
+fn match_structs(){
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    // Try changing the values in the struct to see what happens
+    let foo = Foo { x: (1, 2), y: 3 };
+
+    match foo {
+        Foo { x: (1, b), y } => println!("First of x is 1, b = {},  y = {} ", b, y),
+
+        // you can destructure structs and rename the variables,
+        // the order is not important
+        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+
+        // and you can also ignore some variables:
+        Foo { y, .. } => println!("y = {}, we don't care about x", y),
+        // this will give an error: pattern does not mention field `x`
+        //Foo { y } => println!("y = {}", y);
+    }
+}
+
+fn age() -> u32 {
+    15
+}
+
+fn some_number() -> Option<u32> {
+    Some(42)
+}
+
+fn match_binding(){
+    println!("Tell me what type of person you are");
+
+    match age() {
+        0             => println!("I'm not born yet I guess"),
+        // Could `match` 1 ..= 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 ..= 12. Now the age can be reported.
+        n @ 1  ..= 12 => println!("I'm a child of age {:?}", n),
+        n @ 13 ..= 19 => println!("I'm a teen of age {:?}", n),
+        // Nothing bound. Return the result.
+        n             => println!("I'm an old person of age {:?}", n),
+    }
+
+    match some_number() {
+        // Got `Some` variant, match if its value, bound to `n`,
+        // is equal to 42.
+        Some(n @ 42) => println!("The Answer: {}!", n),
+        // Match any other number.
+        Some(n)      => println!("Not interesting... {}", n),
+        // Match anything else (`None` variant).
+        _            => (),
+    }
+
+}
+
+fn the_match(){
+    for number in 1..=20 {
+
+        print!("Tell me about {}: ", number);
+        match number {
+            // Match a single value
+            1 => println!("One!"),
+            // Match several values
+            2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+            // Match an inclusive range
+            13..=19 => println!("A teen"),
+            // Handle the rest of cases
+            _ => println!("Ain't special"),
+        }
+    }
+
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1,
+        // TODO ^ Try commenting out one of these arms
+    };
+
+    println!("{} -> {}", boolean, binary);
+
+    //the match statement can 'destructure' items in specific ways
+    match_tuples();
+    match_enums();
+    match_pointers_and_references();
+    match_structs();
+
+    //match guards
+    let pair = (2, -2);
+    // TODO ^ Try different values for `pair`
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+
+    match_binding();
+
+}
+
+fn the_if_let(){
+    //from the rust docs
+    //if let permits patterns matching within the condition of an if statement.
+    //This allows us to reduce the overhead of certain kinds of pattern matches
+    //and express them in a more convenient way.
+
+    //this exhaustive match 
+    // match option {
+    //     Some(x) => { foo(x) },
+    //     None => {},
+    // }
+
+    //same as the above match
+    // if let Some(x) = option {
+    //     foo(x);
+    // }
+}
+
+fn the_while_let(){
+    //similar to the if let
+    //used to conditionally loop based on a matched condition
+
+    //awkward match
+    let mut v = vec![1, 3, 5, 7, 11];
+    loop {
+        match v.pop() {
+            Some(x) =>  print!("{} ", x),
+            None => break,
+        }
+    }
+
+    println!("");
+
+    let mut v = vec![1, 3, 5, 7, 11];
+    while let Some(x) = v.pop() {
+        print!("{} ", x);
+    }
+
+    println!("");
+}
 
 fn main() {
     the_if_else();
@@ -190,9 +440,9 @@ fn main() {
 
     the_for_and_range();
 
-    // the_match();
+    the_match();
 
-    // the_if_loop();
+    the_if_let();
 
-    // the_while_let();
+    the_while_let();
 }
